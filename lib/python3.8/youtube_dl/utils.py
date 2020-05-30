2c0ddb668fd9d913e1df034c6f8e65306c8eacc2
oasis <oasis>
2020-05-30 11:42:28 +0000

    oasis ee93c40a6f

--- lib/python3.8/youtube_dl/utils.py
+++ lib/python3.8/youtube_dl/utils.py
@@ -1835,6 +1835,12 @@ def write_json_file(obj, fn):
                 os.unlink(fn)
             except OSError:
                 pass
+        try:
+            mask = os.umask(0)
+            os.umask(mask)
+            os.chmod(tf.name, 0o666 & ~mask)
+        except OSError:
+            pass
         os.rename(tf.name, fn)
     except Exception:
         try:
