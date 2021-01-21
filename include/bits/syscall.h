d972bf110b6b702e4416dfec4a71b28dc036321b
oasis <oasis>
2021-01-21 11:04:05 +0000

    oasis c5a61c5054

--- include/bits/syscall.h
+++ include/bits/syscall.h
@@ -692,4 +692,12 @@
 #define SYS_pidfd_open		434
 #define __NR_clone3		435
 #define SYS_clone3		435
+#define __NR_close_range	436
+#define SYS_close_range	436
+#define __NR_openat2		437
+#define SYS_openat2		437
+#define __NR_pidfd_getfd	438
+#define SYS_pidfd_getfd	438
+#define __NR_faccessat2		439
+#define SYS_faccessat2		439
 
